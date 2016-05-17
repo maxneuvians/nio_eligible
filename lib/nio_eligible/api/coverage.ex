66b2a805367a8a7fe86dd84bf4645b553e428c1f
Max Neuvians <max@neuvians.net>
2016-05-17 15:05:20 -0400

    Added more options to coverage

--- lib/nio_eligible/api/coverage.ex
+++ lib/nio_eligible/api/coverage.ex
@@ -7,6 +7,11 @@ defmodule NioEligible.API.Coverage do
       |> Parser.parse_coverage
   end
 
+  def get(%{payer_id: _, service_provider_organization_name: _, provider_npi: _} = params) do
+    Base.request(:get, "coverage/all.json", params)
+      |> Parser.parse_coverage
+  end
+
   def get(_) do
     "Please enure your map includes :payer_id, :provider_last_name, :provider_first_name, and :provider_npi"
   end
